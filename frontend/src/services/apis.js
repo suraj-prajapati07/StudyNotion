e012ec34860034a2b3636cef24212ec27a9ff7d8
suraj-prajapati07 <sp407522@gmail.com>
2026-01-18 15:42:54 +0530

    Update API base URL for production

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:4000/api/v1";
+// const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = "https://studynotion-aexu.onrender.com"
 
 // CATAGORIES API
 export const categories = {
